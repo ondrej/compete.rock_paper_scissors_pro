f118df8e0413df0f05d4627c1e19d80a2989dec3
Ondrej Hrebicek <ondrej@syncplicity.com>
2011-01-14 09:48:37 -0800

    "Network" bot that will get its move by making an HTTP request.

---
+++ Source/RockPaperScissorsPro.Network/NetworkBot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Compete.Bot;
+using System.Net;
+
+namespace RockPaperScissorsPro.Network
+{
+    public class NetworkPlayerFactory : INetworkBotFactory
+    {
+        public IBot CreateBot(string url)
+        {
+            return new NetworkBot(url);
+        }
+    }
+
+    public class NetworkBot : IRockPaperScissorsBot
+    {
+        Uri url;
+
+        public NetworkBot(string url)
+        {
+            this.url = new Uri(url);
+        }
+
+        public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules, IGameLog log)
+        {
+            /*using (HttpWebRequest request = WebRequest.Create(uri))
+            {
+            }*/
+
+            return Round1Move.Rock;
+        }
+    }
+}
